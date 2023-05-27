490bff85027edba8f1b2ce4d3f30eedc6a32c3ff
davidjaciel <david-1020@live.com.mx>
2023-05-26 23:28:18 -0600

    first commit

---
+++ src/test/java/com/apexdevs/pomodoroApp/PomodoroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexdevs.pomodoroApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

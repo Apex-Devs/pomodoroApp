274a5c755640d0e089f265c3210f35fb784987f7
EnriqueUsanF <95720360+EnriqueUsanF@users.noreply.github.com>
2023-05-30 20:46:06 -0600

    Cambios de nombre

--- apis/account-creation-api/src/main/java/com/apexdevs/accountcreationapi/controllers/AccountsController.java
+++ apis/account-creation-api/src/main/java/com/apexdevs/accountcreationapi/controllers/AccountsController.java
@@ -1,12 +1,20 @@
 package com.apexdevs.accountcreationapi.controllers;
 
+import com.apexdevs.accountcreationapi.model.Accounts;
+import com.apexdevs.accountcreationapi.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class AccountsRepository {
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+//AccountRepository -> AccountsController ----- @Controller -> RestController
+@RestController
+public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
@@ -20,9 +28,20 @@ public class AccountsRepository {
         return "{\"message\": \"Hola\"}";
     }
 
-    @PostMapping("/Post")
-    public String Post() {
-        return "{\"message\": \"Hola\"}";
+    //Metodo post para realizar insercion con Json
+    @PostMapping("/add")
+    public ResponseEntity<String> createAccount(
+            @Validated
+            @RequestBody
+            Accounts accounts,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error en los datos enviados");
+        }
+        accountsRepository.save(accounts);
+        return ResponseEntity.ok("Profesor creado exitosamente");
     }
 
 

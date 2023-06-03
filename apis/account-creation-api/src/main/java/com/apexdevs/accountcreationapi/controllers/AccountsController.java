2865c93581ac6d00d35557997a255afaaf7f9c7d
EnriqueUsanF <95720360+EnriqueUsanF@users.noreply.github.com>
2023-06-02 23:04:15 -0600

    Metodo Post

--- apis/account-creation-api/src/main/java/com/apexdevs/accountcreationapi/controllers/AccountsController.java
+++ apis/account-creation-api/src/main/java/com/apexdevs/accountcreationapi/controllers/AccountsController.java
@@ -2,6 +2,7 @@ package com.apexdevs.accountcreationapi.controllers;
 
 import com.apexdevs.accountcreationapi.model.Accounts;
 import com.apexdevs.accountcreationapi.repository.AccountsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,31 +19,32 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
-    //TODO
-    //Endpoints
-    //name
-    //email
-    //password
-    @GetMapping("/hello")
-    public String hello() {
-        return "{\"message\": \"Hola\"}";
-    }
+    ObjectMapper objectMapper = new ObjectMapper();
 
     //Metodo post para realizar insercion con Json
     @PostMapping("/add")
     public ResponseEntity<String> createAccount(
-            @Validated
-            @RequestBody
-            Accounts accounts,
+            @RequestBody String requestBody,
+            ObjectMapper objectMapper,
             BindingResult bindingResult
     ) {
-        if (bindingResult.hasErrors()) {
+        try {
+            // Deserializar JSON a objeto
+            Accounts accounts = objectMapper.readValue(requestBody, Accounts.class);
+            if (bindingResult.hasErrors()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Error en los datos enviados");
+            }
+            accountsRepository.save(accounts);
+            return ResponseEntity.ok("Profesor creado exitosamente");
+
+            // Hacer algo con el objeto deserializado...
+        } catch (Exception e) {
+            // Manejar la excepción en caso de que ocurra un error durante la deserialización
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Error en los datos enviados");
         }
-        accountsRepository.save(accounts);
-        return ResponseEntity.ok("Profesor creado exitosamente");
     }
-
 
 }

15a462bf8fc22237e2606a7b14ee4ca180ca4d25
EnriqueUsanF <95720360+EnriqueUsanF@users.noreply.github.com>
2023-07-04 19:44:06 -0600

    Creacion de la API de verificacion de cuenta y recuperacion de contrasenia

---
+++ apis/account-verification-api/src/test/java/com/apexdevs/accountverificationapi/AccountVerificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexdevs.accountverificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountVerificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
